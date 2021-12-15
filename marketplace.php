1a45fd9bfd9b4ec5b2bebbe12401a2b54796f8b5
Dev Patel <devdp2001@gmail.com>
2021-12-14 19:57:09 -0500

    updated marketplace.php

--- marketplace.php
+++ marketplace.php
@@ -4,6 +4,9 @@ session_start();
 	include("connection.php");
 	include("functions.php");
 
+    $queryProducts = 'SELECT * FROM products WHERE categoryID = 2 ORDER BY productID';
+    $products = mysqli_query($con, $queryProducts);
+    
 ?>
 
 <!doctype HTML>
@@ -55,7 +58,7 @@ session_start();
                         </a>
                     <?php } ?>
                     <!---cart----->
-                    <a href="cart.html">
+                    <a href="checkoutform.php">
                         <i class="gg-shopping-cart"></i>
                     </a>
                 </div>
@@ -85,6 +88,33 @@ session_start();
             <p id="topbannertitle">MARKETPLACE</p>
         </div>
         <div class="featured-items-container">
+            <div class="featured-container">
+                <?php foreach ($products as $product) : ?>
+                <div class="featured-item">
+                    <div>
+                        <div class="item-info">
+                            <div>
+                                <p class="item=text"><s><?php echo $product['productName']; ?></s></p>
+                            </div>
+                            <div>
+                                <p class="item=text"><s>$<?php echo $product['listPrice']; ?></s></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <img src="<?php echo $product['imagePath']; ?>" alt="red rhude T-shirt"
+                            width="275px"
+                            height="210px"
+                        >
+                    </div>
+                    <div class="button-area">
+                        <?php if ($product['stock'] == 0) { ?>
+                            <button class="featured-out-of-stock">OUT OF STOCK</button>
+                        <?php } ?>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+       <!---  <div class="featured-items-container">
             <div class="featured-container">
                 <div class="featured-item" id="featureditem1">
                     <div>
@@ -316,7 +346,7 @@ session_start();
                     <div class="button-area">
                         <button class="featured-out-of-stock">VIEW</button>
                     </div>
-                </div>
+                </div> --->
                 
                 
             </div>
