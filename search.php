bf8639860b6fe2ad6dfdc4f7f0f504318e062550
ckgluck <ckgluck@gmail.com>
2021-12-14 19:16:02 -0500

    pulled from darius

--- search.php
+++ search.php
@@ -4,17 +4,6 @@ session_start();
 	include("connection.php");
 	include("functions.php");
 
-<<<<<<< HEAD
-?>
-
-<!doctype HTML>
-<html lang="english">
-<head> 
-    <title>Clothes</title>
-    <link rel="shortcut icon" href="images/atllogo.png">
-    <link rel="stylesheet" href="style/normalize.css">
-    <link rel="stylesheet" href="css/home-style.css">
-=======
 ?> 
 
 <!doctype HTML>
@@ -23,24 +12,12 @@ session_start();
     <title>Search</title>
     <link rel="shortcut icon" href="images/atllogo.png">
     <link rel="stylesheet" href="style/normalize.css">
->>>>>>> 1174da62208fa760fbff7151514ca3d263b261fe
     <link rel="stylesheet" href="css/search-style.css">
     <!-----for icons------>
     <link href='https://css.gg/css' rel='stylesheet'>
     <link href='https://unpkg.com/css.gg/icons/all.css' rel='stylesheet'>
     <link href='https://cdn.jsdelivr.net/npm/css.gg/icons/all.css' rel='stylesheet'>
 </head>
-<<<<<<< HEAD
-
-<body>
-    <!--re-search-bar------>
-    <div class="re-search-bar">
-        <!--search-input-->
-        <div class="re-search-input">
-            <form action="search.php" method="GET">
-            <!--input-->
-                <input type="text" name="query" placeholder="Search for Product"/>
-=======
 <body>
     <?php echo $_GET['s_query']; ?>
     <?php if (isset($_GET['s_query'])) {
@@ -60,7 +37,6 @@ session_start();
             <form action="./search.php" method="GET">
             <!--input-->
                 <input type="text" name="s_query" placeholder="Search for Product"/>
->>>>>>> 1174da62208fa760fbff7151514ca3d263b261fe
             </form>
             <!--cancel-button---->
             <a href="home.php" class="search-cancel">
@@ -68,48 +44,6 @@ session_start();
             </a>
         </div>
     </div>
-<<<<<<< HEAD
-
-    <div class="search-results">
-    <?php
-    $s_query = $GET['s-query'];
-
-    //if(!empty($s_query)) {
-        // read from database 
-        $query = "SELECT * FROM 'products' WHERE productName LIKE '%".$s_query."%'";
-        $result_count = mysqli_num_rows($query);
-
-        $result = mysqli_query($con, $query);
-
-        if($result_count == 1) {
-            echo "<p>1 result</p>";
-        }
-        if($result_count > 1) {
-            echo "<p>" .$result_count. " results </p>";
-        }
-
-        echo 'test';
-
-        if($result_count > 0) {
-            echo "<table>";
-            while($row = mysqli_fetch_array($result)) {
-
-                echo "<tr>" . $row['productName']. "</tr>";
-            }
-            echo "</table>";
-        }
-        else {
-            echo "There were no results";
-        }
-
-    //}
-    ?>
-    </div>
-
-</body>
-
-</html>
-=======
     <div class="featured-items-container">
             <div class="featured-container">
                 <?php foreach ($products as $product) : ?>
@@ -144,4 +78,3 @@ session_start();
 </body>
 
 </html>
->>>>>>> 1174da62208fa760fbff7151514ca3d263b261fe
